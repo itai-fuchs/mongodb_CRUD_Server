6768fcdf92fb307998e88de0a1b7be67d83224be
itai <itaifuchs@gmail.com>
2025-08-20 12:21:41 +0300

    update mongo_yaml

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -11,44 +11,26 @@ oc delete secret --all
  oc apply -f mongo_pvc.yaml
  oc apply -f mongo_diployment.yaml
  oc apply -f mongo_service.yaml
-oc rollout status deploy/mongodb -n y0583275590-dev
+
+oc rollout status deploy/mongodb -n itaifuchs-dev
 
 #3
 #build api image
-docker build -t yaakovmor/data-loader-api:mongodb .
-docker push yaakovmor/data-loader-api:mongodb
+docker build -t itaifuchs/data-loader-api:mongodb .
+docker push itaifuchs/data-loader-api:mongodb
 
 
 #4
 # run API
-oc apply -f fastApi_diployment.yaml -n y0583275590-dev
-oc apply -f fastApi_service.yaml -n y0583275590-dev
-oc apply -f fastApi_route.yaml -n y0583275590-dev
-oc rollout status deploy/data-loader-api -n y0583275590-dev
-oc get route data-loader-api -n y0583275590-dev
+oc apply -f fastApi_diployment.yaml -n itaifuchs-dev
+oc apply -f fastApi_service.yaml -n itaifuchs-dev
+oc apply -f fastApi_route.yaml -n itaifuchs-dev
+oc rollout status deploy/data-loader-api -n itaifuchs-dev
+oc get route data-loader-api -n itaifuchs-dev
 
 
 
 
-@REM #5 create documents
-@REM oc exec -n y0583275590-dev deploy/mongodb -- bash -lc '
-@REM /opt/bitnami/mongodb/bin/mongosh "mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@127.0.0.1:27017/${MONGODB_DATABASE}?authSource=${MONGODB_DATABASE}" --quiet <<EOF
-@REM db = db.getSiblingDB("${MONGODB_DATABASE}");
-@REM db.data.insertMany([
-@REM   { first_name: "David", last_name: "Cohen" },
-@REM   { first_name: "Ariel", last_name: "Levy"  },
-@REM   { first_name: "Inon",  last_name: "Mizrahi"},
-@REM   { first_name: "Eyal",  last_name: "Peretz"},
-@REM   { first_name: "Moshe", last_name: "Biton" }
-@REM ]);
-@REM printjson(db.data.countDocuments());
-@REM EOF
-@REM '
-@REM
-@REM oc exec -n y0583275590-dev deploy/mongodb -- bash -lc '
-@REM /opt/bitnami/mongodb/bin/mongosh "mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@127.0.0.1:27017/${MONGODB_DATABASE}?authSource=${MONGODB_DATABASE}" --quiet --eval "db.data.find().limit(5).toArray()"
-@REM '
-@REM
-@REM oc set env deploy/data-loader-api MONGO_COLLECTION=data -n y0583275590-dev
-@REM
-@REM oc rollout restart deploy/data-loader-api -n y0583275590-dev
+oc set env deploy/data-loader-api MONGO_COLLECTION=ARMI -n itaifuchs-dev
+
+oc rollout restart deploy/data-loader-api -n itaifuchs-dev
